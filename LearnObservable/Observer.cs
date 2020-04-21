abd8dfaf5a84142b152478342e59c882fdc49f98
lev19 <levbio@yahoo.com>
2020-04-21 18:06:56 +0300

    Observer

---
+++ LearnObservable/Observer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LearnObservable
+{
+    public class Observer : IObserver<int>
+    {
+        public void OnCompleted()
+        {
+            var ManagedThreadId = Thread.CurrentThread.ManagedThreadId.ToString();
+            Console.WriteLine($"Observation completed, current thread:{ManagedThreadId}");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine($"The error: {error.Message}" );
+        }
+
+        public void OnNext(int value)
+        {
+            var ManagedThreadId = Thread.CurrentThread.ManagedThreadId.ToString();
+            Console.WriteLine($"The value: {value},ManagedThreadId: {ManagedThreadId} ");
+        }
+    }
+}
---
+++ LearnObservable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace LearnObservable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ManagedThreadId = Thread.CurrentThread.ManagedThreadId.ToString();
+            Console.WriteLine($"ManagedThreadId : {ManagedThreadId}");
+            var observable = Observable.Range(5, 9);
+            var subscription = observable.Subscribe(new Observer());
+            subscription.Dispose();
+        }
+    }
+}
